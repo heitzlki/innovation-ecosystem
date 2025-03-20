741f51d10fdf1c8843e214574d484b9a6d1488fa
lukasdiebold <lukas.diebold@gmail.com>
2025-03-20 17:59:33 +0100

    Add Login

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '../components/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
+import { AuthProvider } from './contexts/authContext';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,18 +26,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
-        <Toaster />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang='en' suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+          <Toaster />
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
